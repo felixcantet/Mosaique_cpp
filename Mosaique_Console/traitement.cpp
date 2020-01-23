a9ad7e8ca08e8a2e61389038a2e910a6c2a4d6dd
Sofiane <sofiane.benembareck@hotmail.com>
2020-01-23 15:19:33 +0100

    Update de la fonctione blackOrWhite

--- Mosaique_Console/traitement.cpp
+++ Mosaique_Console/traitement.cpp
@@ -4,7 +4,7 @@
 #include <vector>
 #include "utils.h"
 
-Image* blanckWhite(Image& im)
+Image* luminance(Image& im)
 {
 	Image* newIm = &im;
 
@@ -22,13 +22,14 @@ Image* blanckWhite(Image& im)
 
 Image* blackOrWhite(Image& im)
 {
-	Image* newIm = blanckWhite(im);
+	Image* newIm = new Image(im);
 
 	for (int i = 0; i < newIm->getWidth(); i++)
 	{
 		for (int j = 0; j < newIm->getHeight(); j++)
 		{
-			newIm->pixels[i][j] = newIm->pixels[i][j]->getR() >= 128 ? new Pixel(*new Color(255, 255, 255)) : new Pixel(*new Color(0, 0, 0));
+			float somme = (newIm->pixels[i][j]->getR() + newIm->pixels[i][j]->getG() + newIm->pixels[i][j]->getB()) / 3;
+			newIm->pixels[i][j] = somme >= 127.5f ? new Pixel(*new Color(255, 255, 255)) : new Pixel(*new Color(0, 0, 0));
 		}
 	}
 	
@@ -100,52 +101,3 @@ Image* iterateMeanshift(Image& im, int radius, int ‍‍​‌‌﻿﻿​﻿th
 	}
 	return copy;
 }
-
-/*
- *     for i in range(cop.size[0]):
-            for j in range(cop.size[1]):
-                #Calcule des voisins min et max
-                minX = i - radius;
-                if(minX < 0):
-                    minX = 0
-            
-                minY = j - radius;
-                if(minY < 0):
-                    minY = 0 
-            
-                maxX = i + radius + 1;
-                if(maxX >= cop.size[0]):
-                    maxX = cop.size[0]
-            
-                maxY = j + radius + 1;
-                if(maxY >= cop.size[1]):
-                    maxY = cop.size[1]
-
-                rCrt, gCrt, bCrt = pix[i,j]
-            
-                #recherche des voisins et addition de leur valeur
-                for k in range(minX, maxX):
-                    for l in range (minY, maxY):
-                        r, g, b = pix[k,l‍‍​‌‌﻿﻿​﻿]
-                        norm = abs(rCrt - r) + abs(gCrt - g) + abs(bCrt - b)#math.sqrt(math.pow(r - rCrt, 2) + \
-                                #        math.pow(g - gCrt, 2) +  \
-                                 #       math.pow(b - bCrt, 2))
-                        if(norm < threshold):
-                            rAdd += r
-                            gAdd += g
-                            bAdd += b
-                            voisinAdd += 1
-            
-                #calcule de la nouvelle valeur
-                newValue = (int(rAdd / voisinAdd), int(gAdd /voisinAdd), int(bAdd/voisinAdd))
-                imgPix[i,j] = newValue
-            
-                #re initialisation des voisin
-                voisinAdd = 0
-                rAdd = 0
-                gAdd = 0 
-                bAdd = 0
-                
-    return cop;
-    pass
- */
--- Mosaique_Console/traitement.h
+++ Mosaique_Console/traitement.h
@@ -2,7 +2,7 @@
 #define _TRAITEMENT_HPP_
 #include "Image.h"
 
-Image* blanckWhite(Image& im);
+Image* luminance(Image& im);
 Image* blackOrWhite(Image& im);
 
 Image* meanshift(Image& im, int radius, int ‍‍​‌‌﻿﻿​﻿threshold, int iterations);
